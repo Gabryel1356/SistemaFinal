df268591db3a9f127d58d1bb8dfab6791c281f80
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-02-06 12:12:12 -0500

    (xiomara)Creacion de paquetes y referencias del MSPaciente

---
+++ Ms.Paciente.Aplicacion/Paciente/IPacienteService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ms.Paciente.Aplicacion.Paciente
+{
+    internal class Class1
+    {
+    }
+}
