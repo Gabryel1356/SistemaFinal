0b75542be0ba45f48c7b68b3191ac2dc9f56a171
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-02-05 21:47:25 -0500

    Modificaciones de la estructura

---
+++ Ms.GenerarConsulta.Api/Controllers/GenerarConsultasController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ms.GenerarConsulta.Api.Controllers
+{
+    [ApiController]
+
+    public class GenerarConsultasController : Controller
+    {
+        
+
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GenerarConsultasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GenerarConsultasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GenerarConsultasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenerarConsultasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GenerarConsultasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenerarConsultasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GenerarConsultasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
